2f252ce8552d9ae11ec02f0dcde251a537f73299
Alperen Topcu <alperenttpc78@gmail.com>
2022-11-22 21:07:55 +0300

    new spring project

---
+++ src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -0,0 +1,33 @@
+package kodlamaio.northwind.entities.concretes;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data  // lombok yardımı ile getter ve setter yazmaktan kurtuluyoruz.
+@Entity // sen bir veritabanı nesnesisin demek
+@Table(name = "products") // veritabanında hangi tabloya karşılık geldiğini belirttik
+@AllArgsConstructor  // constructor yazmamak için kullandık
+@NoArgsConstructor  // parametresiz constructor yazmamak için kullandık
+public class Product {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY) // veriler birer birer arttığını gösterir.
+        // @Column(name = "product_id")
+        // private int id;
+        @Column(name = "category_id")
+        private int categoryId;
+        @Column(name = "product_name")
+        private String productName;
+        @Column(name = "unit_price")
+        private double unitPrice;
+        @Column(name = "units_in_stock")
+        private short unitsInStock;
+        @Column(name = "quantity_per_unit")
+        private String quantityPerUnity;
+
+        @ManyToOne
+        @JoinColumn(name = "category_category_id")
+        private Category category;
+
+}
